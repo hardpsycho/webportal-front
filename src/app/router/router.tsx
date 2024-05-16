ae7128dd25a5e2dd26736b76c5bd6268a482c846
hardpsycho <harddeveloper@outlook.com>
2024-05-16 23:42:20 +0300

    feat: добавлен root, navbar and ci/cd

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AboutPage } from '@pages/aboutPage'
 import { MainPage } from '@pages/mainPage'
+import { Root } from '@layouts/root'
 
 export const router = createBrowserRouter([
     {
         path: '/',
+        element: <Root />,
         children: [
             {
                 path: '/',
