b727136ba2d31927edd78a8c1c97c9fd815457e1
hardpsycho <harddeveloper@outlook.com>
2024-05-19 14:22:07 +0300

    feat: добавлена 404й маршрут и пейдж лоадер

---
+++ src/pages/notFoundPage/ui/notFoundPage.tsx
@@ -0,0 +1,17 @@
+import { type FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import styles from './notFoundPage.m.scss'
+
+interface NotFoundPageProps {}
+
+const NotFoundPage: FC<NotFoundPageProps> = () => {
+    const { t } = useTranslation()
+
+    return (
+        <div className={styles.notFoundPage}>
+            <p>{t('not-found-page')}</p>
+        </div>
+    )
+}
+
+export { NotFoundPage }
---
+++ src/shared/ui/pageLoader/pageLoader.tsx
@@ -0,0 +1,23 @@
+import { type FC } from 'react'
+import styles from './pageLoader.m.scss'
+
+interface PageLoaderProps {}
+
+const PageLoader: FC<PageLoaderProps> = () => {
+    return (
+        <div>
+            <svg className={styles.spinner} viewBox="0 0 50 50">
+                <circle
+                    className={styles.path}
+                    cx="25"
+                    cy="25"
+                    r="20"
+                    fill="none"
+                    stroke-width="5"
+                ></circle>
+            </svg>
+        </div>
+    )
+}
+
+export { PageLoader }
